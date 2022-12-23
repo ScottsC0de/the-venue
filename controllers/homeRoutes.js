68c73ab8e9c8b0477f69872144230926973f16d7
Lynn Adele Sadler <lynnadelesadler@yahoo.com>
2022-12-22 21:27:35 -0500

    added more routes and auth util

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { Post, User, Comment, Playlist } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+// more code needs to be added
+
+module.exports = router;
+
