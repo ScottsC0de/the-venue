77eac0cb61f5f3eb75026a2f7e3105ed6a91ab8c
Lynn Adele Sadler <lynnadelesadler@yahoo.com>
2022-12-29 16:04:51 -0500

    can now open web app in local host

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
 const playlistRoutes = require('./playlistRoutes');
 const commentRoutes = require('./commentRoutes');
 
